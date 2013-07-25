37ba7f4e0e02f68628fb02ad09377a4e519466d3
g88623 <steven243@fullsail.edu>
2013-07-25 17:37:26 -0400

    Adding JSON data to my JS file.

--- corbishley_steven_assignment3.js
+++ corbishley_steven_assignment3.js
@@ -1,1 +1,42 @@
-alert("JavaScript works!");
+// Steven Corbishley
+// SDI 1307
+// Project 3
+// My SDI Project 3 JavaScript file.
+
+// JSON Data
+
+var jsonWorkers = {
+	"workers": [
+		{
+			"name": "Bob",
+			"numberOfCases": 0
+		},
+		{
+			"name": "Chris",
+			"numberOfCases": 0
+		},
+		{
+			"name": "Steven",
+			"numberOfCases": 0
+		}
+	]
+};
+
+var jsonQueues = {
+	"queues": [
+		{
+			"name": "Security Cases",
+			"numberOfCases": 0
+		},
+		{
+			"name": "Performance Cases",
+			"numberOfCases": 0
+		},
+		{
+			"name": "Technical Cases",
+			"numberOfCases": 0
+		}
+	]
+};
+
+var 
