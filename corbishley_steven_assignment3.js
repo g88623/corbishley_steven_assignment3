4937c651eab43f50296a392690d9dc6472df2df9
g88623 <steven243@fullsail.edu>
2013-07-25 20:59:03 -0400

    Updated the While Loop and included a Nested Loop.

--- corbishley_steven_assignment3.js
+++ corbishley_steven_assignment3.js
@@ -4,7 +4,7 @@
 // My SDI Project 3 JavaScript file.
 
 // JSON Data
-var jsonWorkers = {
+var json = {
 	"workers": [
 		{
 			"name": "Bob",
@@ -26,23 +26,23 @@ var coworkers = [];
 var casesInQueue = 10;
 
 
-var getWorkers = function(workers){							// Argument: Object
-	var isOutOfOffice = false;								// Local Variable
-	var workersWithQueues = {
-		"name": workers.name,								// Property: String
-		"outOfOffice": false,								// Property: Boolean
-		"numberOfCases": 0,									// Property: Number
+var getWorkers = function(workers){								// Argument: Object
+	var isOutOfOffice = false;									// Local Variable
+	var myCoworker = {
+		"name": workers.name,									// Property: String
+		"outOfOffice": false,									// Property: Boolean
+		"numberOfCases": workers.numberOfCases,					// Property: Number
 		"primaryQueue": {
 			"name": (this.name + "\'s Queue"),
 			"numberOfCases": 0
-		},								// Property: Object
-		"queuesAssigned": [primaryQueue],					// Property: Array
+		},									// Property: Object
+		"queuesAssigned": [],									// Property: Array
 		"workCase": function (){
 			if(this.numberOfCases > 0){
 				this.numberOfCases--;
 				console.log(this.name + " has worked and closed one case.");
 			}
-		},						// Method: Procedure, Math, Output, Back
+		},							// Method: Procedure, Math, Output, Back
 		"setOutOfOffice": function(isOutOfOffice){
 			if(isOutOfOffice===true){              // Conditional
 				if(this.numberOfCases>0){          // Nested Conditional
@@ -55,23 +55,49 @@ var getWorkers = function(workers){							// Argument: Object
 				this.outOfOffice = false;
 				console.log(this.name + " has returned to the office.");
 			}
-		},		// Method: Mutator, Argument: Boolean, Conditional, Nested Conditional
+		},			// Method: Mutator, Argument: Boolean, Conditional, Nested Conditional
 		"isOutOfOffice": function(){
 			return this.outOfOffice;
-		},					// Method: Function, Return Boolean
+		},						// Method: Function, Return Boolean
 		"getStatus": function(){
 			var statusMessage = (this.name + " currently has " + this.numberOfCases + " cases.");
 			console.log(statusMessage);
 			return statusMessage;
-		}						// Method: Accessor, Return String
+		},							// Method: Accessor, Return String
+		"getQueueNames": function(){
+			return queuesAssigned;
+		},						// Return Array
+		"getNumberOfCases": function(){
+			return numberOfCases;
+		}	,					// Return Number
+		"addQueueToAssignedList": function(queueToAdd){
+			queuesAssigned.push(queueToAdd);
+		}	,	// Argument: Array
+		"say": function(message){
+			console.log(this.name + " says " + message);
+		}							// Argument: String
+	}
+	return myCoworker;
+};
+
+
+for(var key in json.workers){									// For Loop
+	coworker = json.workers[key];
+	coworkers.push(coworker);
+	console.log(coworker.name);
+	console.log(coworker);
+	console.log(coworkers);
+}; // For loop, parses through JSON.
+
+while(casesInQueue > coworkers.length) {						// While Loop
+	console.log(casesInQueue + " cases are currently in the queue.");
+	for(var i = 0; i < coworkers.length; i++){					// Nested Loop
+		var myCoworker = getWorkers(coworkers[i]);
+		myCoworker.numberOfCases++;
+		casesInQueue--;
+		console.log(myCoworker.name + " took one case. There are now " + casesInQueue + " cases in the queue.");
+		myCoworker.workCase();
 	}
 };
 
-for(var key in jsonWorkers){
-	coworker = getWorkers(key);
-	coworkers.push(coworker);
-}; // For loop, parses through JSON and sends to getWorkers function.
-
-
-
-console.log(jsonQueues);
+console.log(json);
